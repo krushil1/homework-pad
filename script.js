6d562d168e50f0df90296adf2373376822e6b307
Glitch (krushil1-smstimer-2) <none>
2020-04-17 02:34:37 +0000

    🔖🕷 Checkpoint ./index.html:6631122/74 ./timing.css:6631122/230 ./script.js:6631122/314

--- script.js
+++ script.js
@@ -416,7 +416,7 @@ var realSeconds;
      if (hh > 8) {
       displayPeriod3 = "is Over!";
     } else {
-    displayPeriod3 = "Ends In:&nbsp" + hh + ":" + mm + ":" + realSeconds;
+    displayPeriod3 = "<color="#00000">Ends In:&nbsp</color>" + hh + ":" + mm + ":" + realSeconds;
     }
     setTimeout(period3, 100);
   }
