7f797d96324c4fec56ca226dbb45f83500cd8dcd
Glitch (krushil1-smstimer-2) <none>
2020-04-26 17:28:06 +0000

    🖖🌋 Checkpoint ./timing.css:6631122/11 ./script.js:6631122/350 ./index.html:6631122/836

--- script.js
+++ script.js
@@ -211,8 +211,7 @@ var Text4c = text4c.fontcolor("white");
 
 var end4c = "Lunch 4c is Over!";
 //var End4c = end4c.fontcolor("white");
-var End4c = end4c.fontsize("95px");
-
+var End4c = end4c.fontsize("95%");
 
 
 
