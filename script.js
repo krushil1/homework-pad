d4f2423133b36f48770f77db3757ad117d527336
Glitch (krushil1-smstimer-2) <none>
2020-05-02 22:54:49 +0000

    🚘🍅 Checkpoint ./script.js:6631122/228

--- script.js
+++ script.js
@@ -287,7 +287,8 @@ var realSeconds;
     var mm = pad((remain / 60) % 60);
     var text1 = "Period 1 Ends In: &nbsp" + hh + ":" + mm + ":" + realSeconds; 
     var Text12 = text1.fontcolor("white"); //controls the "Ends In:" text color
-    var Text1 = Text12.fontsize("100px"); 
+    var Text1 = text1.fontSize = "25px";
+    //var Text1 = Text13.big();
     
      if (hh > 8) {
        displayPeriod1 = End1; 
