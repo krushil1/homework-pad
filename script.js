862a98e7f88e327e5f9429aa24abe0c5ec963c34
Glitch (krushil1-smstimer-2) <none>
2020-05-09 03:55:24 +0000

    ✨☺️ Checkpoint ./index.html:6631122/1355 ./script.js:6631122/16

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var preloader = document.getElementById('loading');
+var preloader = document.getElementById('WarpSpeed');
 function myFunction(){
   setTimeout(function(){preloader.style.display = "none"}, 2900);
 }
