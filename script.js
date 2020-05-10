1669f17148665f7b7b7fe74f0ea9c3fedc39b49a
Glitch (krushil1-smstimer-2) <none>
2020-05-10 01:49:25 +0000

    🍵🍗 Checkpoint ./index.html:6631122/497 ./script.js:6631122/16 ./assignments.html:6631122/187

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 
 
-//var preloader = document.getElementById("WarpSpeed");
-//function myFunction() {
- // setTimeout(function() {
-//    preloader.style.display = "none";
-//  }, 2000);
-//}
+var preloader = document.getElementById("WarpSpeed");
+function myFunction() {
+  setTimeout(function() {
+    preloader.style.display = "none";
+  }, 2000);
+}
 
 
 
